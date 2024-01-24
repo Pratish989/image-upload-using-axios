a8cc10ee527e3d13e5d126a114fa706fbf2cab53
Pratish <75656608+Pratish989@users.noreply.github.com>
2024-01-24 18:53:29 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,71 +1,97 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./App.css";
 import axios from "axios";
 
-const baseUrl = "http://192.168.1.209:3000/api/v1/uploadSingleImage";
+const baseUrl = "http://192.168.1.209:3000/api/v1/uploadMultiImage";
 
 function App() {
   const [files, setFiles] = useState([]);
-  const [fileErrors, setFileErrors] = useState([]);
   const [successMessage, setSuccessMessage] = useState(null);
+  const [fileErrors, setFileErrors] = useState([])
+  const [fileErrorMessage, setFileErrorMessage] = useState("");
 
   const onUploadImages = async (e) => {
     e.preventDefault();
-
+    console.log(files);
     const formData = new FormData();
     files.forEach((file) => {
-      formData.append("images", file);
+      formData.append("file", file);
     });
 
     try {
+      console.log({ files });
       const response = await axios.post(baseUrl, formData, {
         headers: {
           "Content-Type": "multipart/form-data",
         },
       });
-
       setSuccessMessage("Images successfully uploaded");
-      console.log(response.data);
+      console.log(response);
     } catch (err) {
       console.log(err.response);
     }
   };
 
+  // const chooseImages = (e) => {
+  //   const selectedFiles = e.target.files;
+  //   const newFiles = [];
+
+  //   if (selectedFiles.length > 5) {
+  //     setFileErrorMessage("You can't choose more than 5 images");
+  //     setSuccessMessage("");
+  //   } else {
+  //     for (let i = 0; i < Math.min(5, selectedFiles.length); i++) {
+  //       const file = selectedFiles[i];
+  //       console.log(file);
+  //       if (file.size > maxAllowedSize) {
+  //         setFileErrorMessage(`${file.name} is too big! Max size allowed up to 5mb`)
+  //       } else if (file.type !== "image/*") {
+  //         setFileErrorMessage(`${file.name} is not an image. Unsupported extension`)
+  //       } else if(file.type === "image/*"){
+  //         newFiles.push(file)
+  //       }
+  //     }
+  //   }
+  // };
+
+
   const chooseImages = (e) => {
     const selectedFiles = e.target.files;
     const newFiles = [];
-    const newErrors = [];
+    const newErrors = []
 
     if (selectedFiles.length > 5) {
-      newErrors.push("You can't choose more than 5 images");
+      setFileErrorMessage("You can't choose more than 5 images")
+
     } else {
       for (let i = 0; i < Math.min(5, selectedFiles.length); i++) {
         const file = selectedFiles[i];
+        console.log(file)
 
         if (file.size > maxAllowedSize) {
-          newErrors.push(`${file.name} is too big! Max size allowed up to 5mb`);
+          setFileErrorMessage(`${file.name} is too big! Max size allowed up to 5mb`)
         } else if (
-          !(
-            file.type === "image/jpg" ||
-            file.type === "image/jpeg" ||
-            file.type === "image/png"
-          )
+          !file.name.match(/\.(jpg|jpeg|png|gif|heic|avif|JPG|JPEG)$/)
         ) {
-          newErrors.push(`${file.name} is not an image. Unsupported extension`);
-        } else {
+          newErrors.push(`\n  ${file.name} is not an image. Unsupported extension! `)
+          // setFileErrorMessage(`${file.name} is not an image. Unsupported extension`)
+        } else  {
           newFiles.push(file);
         }
-      }
+      } 
     }
 
     setFiles(newFiles);
-    setFileErrors(newErrors);
+    setFileErrors(newErrors)
+
   };
 
-  const removeImage = (index) => {
-    const newFiles = [...files];
-    newFiles.splice(index, 1);
 
+  const removeImage = (index) => {
+    console.log(`Image at ${index} removed`);
+    const tempArr = [...files];
+    const newFiles = tempArr.filter((file, i) => i !== index);
+    console.log(newFiles);
     setFiles(newFiles);
   };
 
@@ -73,45 +99,51 @@ function App() {
 
   return (
     <>
-      <div className="upload-section">
-        <input
-          type="file"
-          name="images"
-          id="images"
-          accept="image/*"
-          multiple
-          onChange={chooseImages}
-        />
-        <br />
-        {fileErrors.map((error, index) => (
-          <p key={index} style={{ color: "red" }}>
-            {error}
-          </p>
-        ))}
-        <div className="image-panel">
-          {files.map((file, index) => (
-            <div key={index} className="selected-image">
-              <img
-                src={URL.createObjectURL(file)}
-                alt={file.name}
-                style={{ height: "100px", width: "100px" }}
-              />
-              <button
-                className="remove-button"
-                onClick={() => removeImage(index)}
-              >
-                X
-              </button>
-            </div>
-          ))}
+      <h3>Multiple image </h3>
+      <h4 style={{color:"red"}}>{fileErrors}</h4>
+      <p style={{ color: "greenyellow" }}>{successMessage}</p>
+      <h4 style={{ color: "red" }}>{fileErrorMessage}</h4>
+      <form>
+        <div className="upload-section">
+          <input
+            type="file"
+            name="file"
+            id="images"
+            accept="*"
+            multiple
+            onChange={chooseImages}
+          />
+          <br />
+          <div className="image-panel">
+            {files.map((file, index) => (
+              <div key={index}>
+                <img
+                  src={URL.createObjectURL(file)}
+                  alt={file.name}
+                  style={{ height: "100px", width: "100px" }}
+                />
+                <button
+                  type="button"
+                  key={index}
+                  onClick={() => removeImage(index)}
+                >
+                  X
+                </button>
+              </div>
+            ))}
+          </div>
+          <button
+            disabled={files.length === 0}
+            onClick={onUploadImages}
+            className="upload-button"
+          >
+            Upload
+          </button>
         </div>
-        <button disabled={files.length === 0} onClick={onUploadImages}>
-          Upload
-        </button>
-        <p>{successMessage}</p>
-      </div>
+      </form>
     </>
   );
 }
 
 export default App;
+
